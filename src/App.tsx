d16aa8d654a711cd92e34ef24d5009181510beb8
Alexa Dominguez <alexadominguez@Alexas-MacBook-Pro.local>
2020-10-02 00:42:38 -0500

    testing routing elements and paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
 import * as React from "react";
-import { Redirect, Route, Switch } from "react-router-dom";
+import {
+  // Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
 import { HomeScreen } from "./components/HomeScreen";
 
-const Root: React.FC = () => {
-  return <Redirect to="/sticky_notes_app/homeScreen" />;
-};
+// TO DO: remove comments when other path is added
+// const Root: React.FC = () => {
+//   return <Redirect to="/sticky_notes_app" />;
+// };
 
 export const App: React.FC = () => {
   return (
     <React.Fragment>
       <Switch>
-        <Route exact component={Root} path="/sticky_notes_app" />
-        <Route component={HomeScreen} path="/sticky_notes_app/homeScreen" />
+        {/* <Route exact component={Root} path="/sticky_notes_app" /> */}
+        <Route component={HomeScreen} path="/sticky_notes_app" />
       </Switch>
     </React.Fragment>
   );
