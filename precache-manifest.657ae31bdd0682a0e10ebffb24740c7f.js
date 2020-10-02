092b0d74f252249467997494690b1482a0bf1e68
Alexa Dominguez <alexadominguez@Alexas-MacBook-Pro.local>
2020-10-01 21:53:47 -0500

    Updates

---
+++ precache-manifest.657ae31bdd0682a0e10ebffb24740c7f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ca5c53c5b2d03fdc69fec77263d2eb9",
+    "url": "/sticky_notes_app/index.html"
+  },
+  {
+    "revision": "a131fe87dae9a1307c86",
+    "url": "/sticky_notes_app/static/css/2.3c845d0b.chunk.css"
+  },
+  {
+    "revision": "b3ecd23b6d32cc776c67",
+    "url": "/sticky_notes_app/static/css/main.82ed3c94.chunk.css"
+  },
+  {
+    "revision": "a131fe87dae9a1307c86",
+    "url": "/sticky_notes_app/static/js/2.f3679718.chunk.js"
+  },
+  {
+    "revision": "8427b85afa7bfb9a3c9fba4555b24402",
+    "url": "/sticky_notes_app/static/js/2.f3679718.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3ecd23b6d32cc776c67",
+    "url": "/sticky_notes_app/static/js/main.fcc9789d.chunk.js"
+  },
+  {
+    "revision": "1c0665d21ca98689a631",
+    "url": "/sticky_notes_app/static/js/runtime-main.a1d04b01.js"
+  },
+  {
+    "revision": "f8de91df6cd020f36422fd1dcee2560b",
+    "url": "/sticky_notes_app/static/media/default.f8de91df.png"
+  },
+  {
+    "revision": "74938f2fed0dfe87e623c54423698848",
+    "url": "/sticky_notes_app/static/media/pageNotFound.74938f2f.png"
+  }
+]);
