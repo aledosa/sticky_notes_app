722f2b734687b9840183223f72205f9c5895fa5d
Alexa Dominguez <alexadominguez@Alexas-MacBook-Pro.local>
2020-10-02 01:03:29 -0500

    changed to final path

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,16 @@
 import * as React from "react";
-import {
-  // Redirect,
-  Route,
-  Switch,
-} from "react-router-dom";
+import { Redirect, Route, Switch } from "react-router-dom";
 import { HomeScreen } from "./components/HomeScreen";
 
-// TO DO: remove comments when other path is added
-// const Root: React.FC = () => {
-//   return <Redirect to="/sticky_notes_app" />;
-// };
+const Root: React.FC = () => {
+  return <Redirect to="/sticky_notes_app" />;
+};
 
 export const App: React.FC = () => {
   return (
     <React.Fragment>
       <Switch>
-        {/* <Route exact component={Root} path="/sticky_notes_app" /> */}
+        <Route exact component={Root} path="/" />
         <Route component={HomeScreen} path="/sticky_notes_app" />
       </Switch>
     </React.Fragment>
