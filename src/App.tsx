55bdbf60666b180d2b612ab316ca9304e75d6211
Alexa Dominguez <alexadominguez@Alexas-MacBook-Pro.local>
2020-10-02 00:32:23 -0500

    edited paths for gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,15 @@ import { Redirect, Route, Switch } from "react-router-dom";
 import { HomeScreen } from "./components/HomeScreen";
 
 const Root: React.FC = () => {
-  return <Redirect to="/homeScreen" />;
+  return <Redirect to="/sticky_notes_app/homeScreen" />;
 };
 
 export const App: React.FC = () => {
   return (
     <React.Fragment>
       <Switch>
-        <Route exact component={Root} path="/" />
-        <Route component={HomeScreen} path="/homeScreen" />
+        <Route exact component={Root} path="/sticky_notes_app" />
+        <Route component={HomeScreen} path="/sticky_notes_app/homeScreen" />
       </Switch>
     </React.Fragment>
   );
